7e4246a477f9a6afc2d43b2541f28fa226ede795
Nicolas <na.rodriguezp@uniandes.edu.co>
2023-11-27 23:05:34 -0500

    Data Collection without testing

--- data_collection.py
+++ data_collection.py
@@ -1,62 +1,63 @@
 import paho.mqtt.publish as publish
 import paho.mqtt.client as paho
 import random
+import json, dataclasses
+import time
 
-panels = ["panel1", "panel2", "panel3", "panel4"]
-motors = ["motor1", "motor2", "motor3", "motor4"]
+trackers = ["seguidor1", "seguidor2", "seguidor3", "seguidor4"]
 device_ports = [1,2,3,4]
 topics = []
-panel_variables = ["Irradiancia", "Temperatura ambiente", "Voltaje panel", "Corriente panel"]
-motor_variables = ["Angulo elevacion", "Angulo acimutal", "Voltaje motor", "Corriente motor"]
+variable_names = ["Irradiancia", "Temperatura ambiente", "Voltaje panel", "Corriente panel", "Angulo elevacion", "Angulo acimutal", "Voltaje motor", "Corriente motor"]
+to_send_values = {}
+
+# Configura los parámetros
+BROKER = "172.24.100.204"
+port = 8081
+username = ""
+password = ""
+
+client1 = paho.Client("raspberry")
+client1.connect(BROKER, port)
+print("Connected to MQTT Broker")
 
 def build_topics():
-    
-    for i in range(len(panels)):
+    for i in range(len(trackers)):
         current_port = device_ports[random.randint(0, len(device_ports)-1)]
-        topics.append(panels[i] + "/" + str(current_port))
-        topics.append(motors[i] + "/" + str(current_port))
+        topics.append(trackers[i] + "/" + str(current_port))
         device_ports.remove(current_port)
+
+
+class JSONEncoder(json.JSONEncoder):
+        def default(self, o):
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            try:
+                return super().default(o)
+            except TypeError:
+                return str(o)
         
 
-def send_variables(variable):
+def assign_variables():
     irradiancia = random.randint(0,1500)
     tempr = random.randint(-10,70)
     volt_panel = random.randint(0,25)
     corr_panel = random.randint(0,5)
-
     ang_elev = random.randint(0,90)
     ang_acimut = random.randint(0,360)
     volt_motor = random.randint(0,24)
     corr_motor = random.randint(0,3)
+    
+    var_values = [irradiancia, tempr, volt_panel, corr_panel, ang_elev, ang_acimut, volt_motor, corr_motor]
 
+    for i in range (len(variable_names)):
+        to_send_values[variable_names[i]] = var_values[i]
     
 
-
-    #Tengo que usar diccionarios para enviar variable:valor a formato JSON -> Guiarme de monitor.py Juan
-    #El metodo publish de paho.mqtt al parecer me permite formatear diccionarios a JSON!
-    print("kha")
-
-
-
-
-
-
-build_topics()
-for i in range(len(topics)):
-    print("El tema #" +str(i)+ " es " + topics[i])
-
-
-
-# Configura los parámetros
-#broker_address = "172.24.100.204"
-#port = 8081
-#message = "<mensaje>"
-#username = ""
-#password = ""
-
-#client1 = paho.Client("raspberry")
-#client1.connect(broker_address, port)
-
+def send_variables(client):
+    #MIRAR SI SE PUEDE HACER MAS REALISTA MIRANDO LA HORA DEL DIA PARA ESTIMAR LAS VARIABLES.
+    while True:
+        client.publish(topics[random.randint(0,len(topics-1))], json.dumps(to_send_values, cls=JSONEncoder))
+        time.sleep(250)
 
 # Publica el mensaje
 #publish.single(topic, message, hostname=broker_address, auth={'username': username, 'password': password})
